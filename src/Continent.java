8758316c49230eeeb712a8be0148a0339dcb743d
justgo13 <jgao2299@gmail.com>
2020-10-16 17:25:37 -0400

    Configuration of troop placement for variable number of players

--- src/Continent.java
+++ src/Continent.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Continent {
+    private String name;
+    private int bonusArmy;
+    private ArrayList<Country> countries;
+
+    public Continent(String name, int bonusArmy) {
+        this.name = name;
+        this.bonusArmy = bonusArmy;
+        countries = new ArrayList<>();
+    }
+
+    public void addCountry(Country country) {
+
+    }
 }
