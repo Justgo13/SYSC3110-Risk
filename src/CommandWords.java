623ff56af3f1a8aef570757d2ca58a3d62c9fc65
cheet <cheetahdeath@hotmail.com>
2020-10-18 11:23:55 -0400

    CommandWords, Command and Parser

---
+++ src/CommandWords.java
@@ -0,0 +1,40 @@
+public class CommandWords {
+    // an array that holds constant valid command words
+    private static final String[] validCommands = {
+            "attack", "defend", "quit", "help"
+    };
+
+    /*
+    Constructor intitialising the commmand words
+     */
+    public CommandWords(){
+        //do nothing for now...
+    }
+
+    /**
+     * Verifies if the user has used a word that is inside the validCommands
+     * arrayList
+     * @param word is the input String by the user
+     * @return true if valid command, false otherwise
+     */
+    public Boolean isValid(String word){
+        for(int i = 0; i< validCommands.length; i++){
+            if(validCommands[i].equals(word)){
+                //if the word is in the arrayList then return true
+                return true;
+            }
+        }
+        //if the word is not found in the arrayList then return false
+        return false;
+    }
+
+    /**
+     * Outputs all command words in the validCommands
+     * arrayList
+     */
+    public void showCommads(){
+        for(String command: validCommands){
+            System.out.print(command + ", ");
+        }
+    }
+}
--- src/RiskGame.java
+++ src/RiskGame.java
@@ -3,12 +3,15 @@ import java.util.*;
 public class RiskGame {
     private Board board;
     private static final int[] armySize = {50,35,30,25,20};
+    private CommandWords command;
 
     /**
      * Creates an instance of the Risk game
      */
-    public RiskGame() {
+    public RiskGame()
+    {
         board = new Board();
+        command = new CommandWords();
     }
 
     public void attack(Country A, Country B, int numOfAttackers) {
@@ -25,11 +28,12 @@ public class RiskGame {
         board.randomizePlayers(); // randomizes player order
         board.buildMap(); // adds all countries to map
         board.placePlayers(numPlayers); // place players randomly on the map
-        board.setAdjacentCountries();
+        //board.setAdjacentCountries();
+        command.showCommads();
     }
 
     public static void main (String[] args) {
         RiskGame game = new RiskGame();
-        //game.playGame();
+        game.playGame();
     }
 }
