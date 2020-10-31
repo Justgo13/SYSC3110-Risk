1116f3626e6011f66c796692c381198db1131001
justgo13 <jgao2299@gmail.com>
2020-10-31 10:43:20 -0400

    Commented out unessecary dice roll button and enabled attack button

--- src/RiskFrame.java
+++ src/RiskFrame.java
@@ -11,18 +11,23 @@ public class RiskFrame extends JFrame {
         // Creates the control panel at the bottom of the GUI
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints controlPanelConstraints = new GridBagConstraints();
-        controlPanelConstraints.gridx = 0;
-        controlPanelConstraints.gridy = 0;
-        controlPanelConstraints.gridheight = 2;
+        // TODO Do we need the dice roll button?
+//        controlPanelConstraints.gridx = 0;
+//        controlPanelConstraints.gridy = 0;
+//        controlPanelConstraints.gridheight = 2;
+//        controlPanelConstraints.weighty = 1.0;
+//        controlPanelConstraints.weightx = 1.0;
+//        controlPanelConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+//        controlPanelConstraints.fill = GridBagConstraints.BOTH;
+//        JButton diceButton = new JButton("Roll die");
+//        diceButton.setEnabled(false);
+//        panel.add(diceButton, controlPanelConstraints);
+
         controlPanelConstraints.weighty = 1.0;
         controlPanelConstraints.weightx = 1.0;
         controlPanelConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
         controlPanelConstraints.fill = GridBagConstraints.BOTH;
-        JButton diceButton = new JButton("Roll die");
-        diceButton.setEnabled(false);
-        panel.add(diceButton, controlPanelConstraints);
-
-        controlPanelConstraints.gridx = 1;
+        controlPanelConstraints.gridx = 0;
         controlPanelConstraints.gridy = 0;
         controlPanelConstraints.gridwidth = 3;
         controlPanelConstraints.gridheight = 1;
@@ -31,7 +36,7 @@ public class RiskFrame extends JFrame {
         console.setEditable(false);
         panel.add(console, controlPanelConstraints);
 
-        controlPanelConstraints.gridx = 1;
+        controlPanelConstraints.gridx = 0;
         controlPanelConstraints.gridy = 1;
         controlPanelConstraints.gridwidth = 1;
         controlPanelConstraints.anchor = GridBagConstraints.LAST_LINE_END;
@@ -39,14 +44,14 @@ public class RiskFrame extends JFrame {
         reinforce.setEnabled(false);
         panel.add(reinforce, controlPanelConstraints);
 
-        controlPanelConstraints.gridx = 2;
+        controlPanelConstraints.gridx = 1;
         controlPanelConstraints.gridy = 1;
         controlPanelConstraints.anchor = GridBagConstraints.LAST_LINE_END;
         JButton attack = new JButton("ATTACK");
-        attack.setEnabled(false);
+        attack.setEnabled(true);
         panel.add(attack, controlPanelConstraints);
 
-        controlPanelConstraints.gridx = 3;
+        controlPanelConstraints.gridx = 2;
         controlPanelConstraints.gridy = 1;
         controlPanelConstraints.anchor = GridBagConstraints.LAST_LINE_END;
         JButton endturn = new JButton("END TURN");
