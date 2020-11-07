af59ed0de34a38923336aaa9fedf1d5f34dccde3
Shashaank Srivastava <Shankisri@gmail.com>
2020-11-06 19:12:01 -0500

    Added a barebones test class

---
+++ src/RiskModelTest.java
@@ -0,0 +1,68 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RiskModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /*
+    @Test
+    public void getCountries() {
+    }
+
+    @Test
+    public void attack() {
+    }
+
+    @Test
+    public void defendingDiceInitialization() {
+    }
+
+    @Test
+    public void playGame() {
+    }
+
+    @Test
+    public void addRiskView() {
+    }
+
+    @Test
+    public void updateAttackView() {
+    }
+
+    @Test
+    public void updateBattleResults() {
+    }
+
+    @Test
+    public void updateDiceResult() {
+    }
+
+    @Test
+    public void updateCountryLost() {
+    }
+
+    @Test
+    public void endTurnPhase() {
+    }
+
+    @Test
+    public void getBoard() {
+    }
+
+    @Test
+    public void getTurnIndex() {
+    }
+
+    @Test
+    public void incrementTurnIndex() {
+    }
+     */
+}
